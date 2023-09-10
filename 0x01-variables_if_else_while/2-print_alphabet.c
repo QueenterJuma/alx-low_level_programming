614fa7519258b0b09975980ab0f007f8863f2346
QueenterJuma <jumahqueenter@gmail.com>
2023-09-10 11:46:42 -0700

    Add print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int j;
+	for (j = 100; j <= 150; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+
+	return (0);
+}
