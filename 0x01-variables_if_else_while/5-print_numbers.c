618d089a15331ce8e9742e1bcfd3693b440f3993
QueenterJuma <jumahqueenter@gmail.com>
2023-09-11 14:49:39 -0700

    Add update

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,10 +6,13 @@
  */
 int main(void)
 {
-	int i;
+	int i = 48;
 
-	for (i = 0; i < 10; i)
-		printf("%d", i)
-	printf("\n");
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
